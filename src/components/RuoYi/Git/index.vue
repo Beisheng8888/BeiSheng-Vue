9186be5e30b095f4fba010843d38f3e670949c2b
北省 <jih6666@126.com>
2023-02-02 11:54:00 +0800

    标题修改

--- src/components/RuoYi/Git/index.vue
+++ src/components/RuoYi/Git/index.vue
@@ -6,10 +6,10 @@
 
 <script>
 export default {
-  name: 'RuoYiGit',
+  name: 'BeiShengGit',
   data() {
     return {
-      url: 'https://gitee.com/y_project/RuoYi-Vue'
+      url: 'https://github.com/Beisheng8888/BeiSheng-Admin'
     }
   },
   methods: {
