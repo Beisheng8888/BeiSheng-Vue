173cfdb7ca630e1717f09ff734a49f716911efa1
北省 <jih6666@126.com>
2023-02-16 14:13:06 +0800

    添加滑块成功

--- src/views/login_back.vue
+++ src/views/login_back.vue
@@ -9,7 +9,7 @@
           auto-complete="off"
           placeholder="账号"
         >
-          <svg-icon slot="prefix" icon-class="user" class="el-input__icon input-icon"/>
+          <svg-icon slot="prefix" icon-class="user" class="el-input__icon input-icon" />
         </el-input>
       </el-form-item>
       <el-form-item prop="password">
@@ -20,16 +20,23 @@
           placeholder="密码"
           @keyup.enter.native="handleLogin"
         >
-          <svg-icon slot="prefix" icon-class="password" class="el-input__icon input-icon"/>
+          <svg-icon slot="prefix" icon-class="password" class="el-input__icon input-icon" />
         </el-input>
       </el-form-item>
-      <Verify
-        @success="capctchaCheckSuccess"
-        :mode="'pop'"
-        :captchaType="'blockPuzzle'"
-        :imgSize="{ width: '330px', height: '155px' }"
-        ref="verify"
-      ></Verify>
+      <el-form-item prop="code" v-if="captchaEnabled">
+        <el-input
+          v-model="loginForm.code"
+          auto-complete="off"
+          placeholder="验证码"
+          style="width: 63%"
+          @keyup.enter.native="handleLogin"
+        >
+          <svg-icon slot="prefix" icon-class="validCode" class="el-input__icon input-icon" />
+        </el-input>
+        <div class="login-code">
+          <img :src="codeUrl" @click="getCode" class="login-code-img"/>
+        </div>
+      </el-form-item>
       <el-checkbox v-model="loginForm.rememberMe" style="margin:0px 0px 25px 0px;">记住密码</el-checkbox>
       <el-form-item style="width:100%;">
         <el-button
@@ -55,84 +62,96 @@
 </template>
 
 <script>
+import { getCodeImg } from "@/api/login";
 import Cookies from "js-cookie";
-import {encrypt, decrypt} from "@/utils/jsencrypt";
-import Verify from "@/components/Verifition/Verify";
+import { encrypt, decrypt } from '@/utils/jsencrypt'
 
 export default {
-  components: {Verify},
   name: "Login",
   data() {
     return {
+      codeUrl: "",
       loginForm: {
         username: "",
         password: "",
         rememberMe: false,
         code: "",
+        uuid: ""
       },
       loginRules: {
         username: [
-          {required: true, trigger: "blur", message: "请输入您的账号"}
+          { required: true, trigger: "blur", message: "请输入您的账号" }
         ],
         password: [
-          {required: true, trigger: "blur", message: "请输入您的密码"}
+          { required: true, trigger: "blur", message: "请输入您的密码" }
         ],
+        code: [{ required: true, trigger: "change", message: "请输入验证码" }]
       },
       loading: false,
+      // 验证码开关
+      captchaEnabled: true,
       // 注册开关
-      register: false,
+      register: true,
       redirect: undefined
     };
   },
   watch: {
     $route: {
-      handler: function (route) {
+      handler: function(route) {
         this.redirect = route.query && route.query.redirect;
       },
       immediate: true
     }
   },
   created() {
+    this.getCode();
     this.getCookie();
   },
   methods: {
+    getCode() {
+      getCodeImg().then(res => {
+        this.captchaEnabled = res.captchaEnabled === undefined ? true : res.captchaEnabled;
+        if (this.captchaEnabled) {
+          this.codeUrl = "data:image/gif;base64," + res.img;
+          this.loginForm.uuid = res.uuid;
+        }
+      });
+    },
     getCookie() {
       const username = Cookies.get("username");
       const password = Cookies.get("password");
-      const rememberMe = Cookies.get("rememberMe");
+      const rememberMe = Cookies.get('rememberMe')
       this.loginForm = {
         username: username === undefined ? this.loginForm.username : username,
         password: password === undefined ? this.loginForm.password : decrypt(password),
         rememberMe: rememberMe === undefined ? false : Boolean(rememberMe)
       };
     },
-    capctchaCheckSuccess(params) {
-      this.loginForm.code = params.captchaVerification;
-      this.loading = true;
-      if (this.loginForm.rememberMe) {
-        Cookies.set("username", this.loginForm.username, {expires: 30});
-        Cookies.set("password", encrypt(this.loginForm.password), {expires: 30,});
-        Cookies.set("rememberMe", this.loginForm.rememberMe, {expires: 30});
-      } else {
-        Cookies.remove("username");
-        Cookies.remove("password");
-        Cookies.remove("rememberMe");
-      }
-      this.$store.dispatch("Login", this.loginForm).then(() => {
-        this.$router.push({path: this.redirect || "/"}).catch(() => {
-        });
-      }).catch(() => {
-        this.loading = false;
-      });
-    },
     handleLogin() {
-      this.$refs.loginForm.validate((valid) => {
+      this.$refs.loginForm.validate(valid => {
         if (valid) {
-          this.$refs.verify.show();
+          this.loading = true;
+          if (this.loginForm.rememberMe) {
+            Cookies.set("username", this.loginForm.username, { expires: 30 });
+            Cookies.set("password", encrypt(this.loginForm.password), { expires: 30 });
+            Cookies.set('rememberMe', this.loginForm.rememberMe, { expires: 30 });
+          } else {
+            Cookies.remove("username");
+            Cookies.remove("password");
+            Cookies.remove('rememberMe');
+          }
+          this.$store.dispatch("Login", this.loginForm).then(() => {
+            this.$router.push({ path: this.redirect || "/" }).catch(()=>{});
+          }).catch(() => {
+            this.loading = false;
+            if (this.captchaEnabled) {
+              this.getCode();
+            }
+          });
         }
       });
-    },
-  },
+    }
+  }
 };
 </script>
 
@@ -145,7 +164,6 @@ export default {
   background-image: url("../assets/images/login-background.jpg");
   background-size: cover;
 }
-
 .title {
   margin: 0px auto 30px auto;
   text-align: center;
@@ -157,28 +175,32 @@ export default {
   background: #ffffff;
   width: 400px;
   padding: 25px 25px 5px 25px;
-
   .el-input {
     height: 38px;
-
     input {
       height: 38px;
     }
   }
-
   .input-icon {
     height: 39px;
     width: 14px;
     margin-left: 2px;
   }
 }
-
 .login-tip {
   font-size: 13px;
   text-align: center;
   color: #bfbfbf;
 }
-
+.login-code {
+  width: 33%;
+  height: 38px;
+  float: right;
+  img {
+    cursor: pointer;
+    vertical-align: middle;
+  }
+}
 .el-login-footer {
   height: 40px;
   line-height: 40px;
@@ -190,5 +212,8 @@ export default {
   font-family: Arial;
   font-size: 12px;
   letter-spacing: 1px;
+}
+.login-code-img {
+  height: 38px;
 }
 </style>
